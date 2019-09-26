4a8fd95c182d4be112c24a698e07fd1709f997c6
Tom Farmer <farmergeddon@gmail.com>
2019-09-26 17:05:33 +0100

    Adding missing constructors to Map and Set

--- src/tfMap.hpp
+++ src/tfMap.hpp
@@ -8,6 +8,8 @@ namespace tf
 	class Map : public std::map<TKey, TValue>
 	{
 	public:
+		using std::map<TKey, TValue>::map;
+
 		bool contains(const TKey& key) const
 		{
 			return this->find(key) != this->end();
--- src/tfSet.hpp
+++ src/tfSet.hpp
@@ -7,6 +7,9 @@ namespace tf
 	template <typename T>
 	class Set : public std::set<T>
 	{
+	public:
+		using std::set<T>::set;
+
 		bool Contains(const T& key) const
 		{
 			return this->find(key) != this->end();
