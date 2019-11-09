30935035fb08720c1a2e1e82812252bf148cff81
Tom Farmer <farmergeddon@gmail.com>
2019-11-09 14:05:14 +0000

    Added SecureRandomBytes

---
+++ src/tfSecureRandomBytes.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#ifdef _WIN32
+# include <windows.h>
+# define RtlGenRandom SystemFunction036
+# if defined(__cplusplus)
+extern "C"
+# endif
+BOOLEAN NTAPI RtlGenRandom(PVOID RandomBuffer, ULONG RandomBufferLength);
+# pragma comment(lib, "advapi32.lib")
+#else
+#endif
+
+#include "tfExternalLibraryException.hpp"
+#include <type_traits>
+#include <limits>
+#include <cstdint>
+
+namespace tf
+{
+	class SecureRandomBytes
+	{
+	public:
+		static void get(void* buffer, size_t size)
+		{
+			#ifdef _WIN32
+
+			if (!RtlGenRandom(buffer, ULONG(size)))
+			{
+				throw ExternalLibraryException("RtlGenRandom failed");	
+			}
+
+			#else
+			#endif
+		}
+
+		template<typename T>
+		static T get()
+		{
+			T value;
+
+			get(&value, sizeof(value));
+
+			return value;
+		}
+	};
+}
